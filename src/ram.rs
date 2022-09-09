ecd17dd2b7d6e67bcb058ddcce50c04891f775bb
Samarth1696 <samarthrobo03@gmail.com>
2022-09-10 00:59:55 +0530

    Done little Implementation of RAM

--- src/ram.rs
+++ src/ram.rs
@@ -3,18 +3,47 @@ pub struct Ram {
 }
 
 impl Ram {
-
     pub fn new() -> Ram {
-        Ram { 
-            mem: [0; 4096] 
+        let mut ram = Ram { mem: [0; 4096] };
+
+        let sprites: [[u8; 5]; 16] = [
+            [0xF0, 0x90, 0x90, 0x90, 0xF0], //0
+            [0x20, 0x60, 0x20, 0x20, 0x70], //1
+            [0xF0, 0x10, 0xF0, 0x10, 0xF0], //2
+            [0xF0, 0x10, 0xF0, 0x10, 0xF0], //3
+            [0x90, 0x90, 0xF0, 0x10, 0x10], //4
+            [0xF0, 0x80, 0xF0, 0x10, 0xF0], //5
+            [0xF0, 0x80, 0xF0, 0x90, 0xF0], //6
+            [0xF0, 0x10, 0x20, 0x40, 0x40], //7
+            [0xF0, 0x90, 0xF0, 0x90, 0xF0], //8
+            [0xF0, 0x90, 0xF0, 0x10, 0xF0], //9
+            [0xF0, 0x90, 0xF0, 0x90, 0x90], //A
+            [0xE0, 0x90, 0xE0, 0x90, 0xE0], //B
+            [0xF0, 0x80, 0x80, 0x80, 0xF0], //C
+            [0xE0, 0x90, 0x90, 0x90, 0xE0], //D
+            [0xF0, 0x80, 0xF0, 0x80, 0xF0], //E
+            [0xF0, 0x80, 0xF0, 0x80, 0x80], //F
+        ];
+        let mut i=0;
+        for sprite in sprites.iter() {
+            for ch in sprite {
+                ram.mem[i] = *ch;
+                i += 1;
+            }
         }
+
+        for i in 0..0x1ff {
+            print!("{:#X}, ", ram.mem[i]);
+        }
+
+        ram
     }
 
-    pub fn write_byte(&mut self, address: u16, value: u8){
-
+    pub fn write_byte(&mut self, address: u16, value: u8) {
+        self.mem[address as usize] = value;
     }
 
-    pub fn read_byte(&mut self, address: u16, value: u8){
-
+    pub fn read_byte(&mut self, address: u16, value: u8) -> u8 {
+        self.mem[address as usize]
     }
 }
