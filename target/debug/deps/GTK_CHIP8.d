68596b8be59c954a0c7279043c901e302031b88a
Samarth1696 <samarthrobo03@gmail.com>
2022-09-11 10:18:34 +0530

    Added CPU interface

--- target/debug/deps/GTK_CHIP8.d
+++ target/debug/deps/GTK_CHIP8.d
@@ -1,7 +1,8 @@
-C:\Users\SAMARTH\Desktop\rustprojects\GTK-CHIP8\target\debug\deps\GTK_CHIP8.exe: src\main.rs src\ram.rs src\chip8.rs
+C:\Users\SAMARTH\Desktop\rustprojects\GTK-CHIP8\target\debug\deps\GTK_CHIP8.exe: src\main.rs src\ram.rs src\cpu.rs src\chip8.rs
 
-C:\Users\SAMARTH\Desktop\rustprojects\GTK-CHIP8\target\debug\deps\GTK_CHIP8.d: src\main.rs src\ram.rs src\chip8.rs
+C:\Users\SAMARTH\Desktop\rustprojects\GTK-CHIP8\target\debug\deps\GTK_CHIP8.d: src\main.rs src\ram.rs src\cpu.rs src\chip8.rs
 
 src\main.rs:
 src\ram.rs:
+src\cpu.rs:
 src\chip8.rs:
